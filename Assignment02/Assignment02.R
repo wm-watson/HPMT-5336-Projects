966c639570f25f0e76c8f17f269ea0e1a242ffd1
wm-watson <watsonwp@gmail.com>
2024-02-08 16:53:01 -0600

    Save changes

--- Assignment02/Assignment02.R
+++ Assignment02/Assignment02.R
@@ -6,7 +6,6 @@ library(stringr)
 library(ggplot2)
 library(scales)
 library(readxl)
-install.packages("cld2")
 library(cld2)
 library(stopwords)
 
@@ -33,12 +32,10 @@ covid_tidy_stop <- span_covid %>%
   summarise(total = n()) %>%
   ungroup()
 
-detect_language_mixed(covid_tidy_stop$Abstract)
 
 ### Spanish/English Stopwords----
 
 stop_es <- tibble(word=stopwords::stopwords('es'))
-
 
 stop_en <- tibble(word=stopwords::stopwords('en'))
 
@@ -52,3 +49,56 @@ toks <- covid_tidy_stop %>%
 toks_plot_20 <- toks %>%
   arrange(desc(total)) %>%
   slice_head(n = 20)
+
+
+ toks_plot_20 %>%
+  ggplot(aes( x = reorder(word, total), y = total)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() + 
+  theme_minimal() +
+  labs(title = "Barplot of Spanish Words: No Stopwords",
+       x = NULL,
+       y = "Total Count") +
+  scale_y_continuous(labels = scales::comma)
+
+print(toks_plot_20)
+
+rm(toks_plot_20)
+rm(covid_tidy_stop)
+rm(span_covid)
+rm(stop_es)
+rm(toks)
+# Part2----
+
+## English Abstracts----
+covid_eng <- covid %>%
+  filter(Language == 'en')
+
+##Tokenize----
+covid_tidy_stop <- covid_eng %>%
+  unnest_tokens(word, Abstract) %>%
+  group_by(word) %>%
+  summarise(total = n()) %>%
+  ungroup()
+
+##Remove English Stopwords----
+toks <- covid_tidy_stop %>%
+  anti_join(stop_en)
+
+## China Words----
+chin_toks <- toks %>%
+  group_by(word) %>%
+  mutate(china_count = cumsum(str_detect(word,regex("^[Cc]hin[ae][a-z]*",
+                                                    ignore_case = TRUE))))%>%
+    filter(china_count==1)%>%
+    ungroup()
+
+##Italy words----
+ital_toks <- toks %>%
+  group_by(word) %>%
+  mutate(ital_count = cumsum(str_detect(word,regex("^[Ii]tal[a-z]*",
+                                                    ignore_case = TRUE))))%>%
+  filter(ital_count==1)%>%
+  ungroup()
+  
